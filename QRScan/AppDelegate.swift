2bb5f38e8b0db603655d7cf51f9c1eb48aa7c9c7
ChengLuffy <chengluffy@hotmail.com>
2016-10-08 11:41:47 +0800

    Initial Commit

---
+++ QRScan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QRScan
+//
+//  Created by 成璐飞 on 2016/10/8.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QRScan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QRScan
+//
+//  Created by 成璐飞 on 2016/10/8.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
